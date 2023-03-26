08e526c158495cfb3166485d29281ade8a9260ec
SharmaTraders-EV <Sharma.traders.ev@gmail.com>
2023-03-27 00:02:02 +0530

    del

--- bill/billApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Product,Bill,CoustomersData
-
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Bill)
-admin.site.register(CoustomersData)
